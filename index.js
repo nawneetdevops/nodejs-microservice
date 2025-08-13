8ed97b125b9bf077fb1547d79df843976b5430e9
nawneetviplav <102345883+nawneetdevops@users.noreply.github.com>
2025-08-13 11:25:40 +0530

    update

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Demo app is running...</h1>');
+  res.send('<h1>Test app is running...</h1>');
  })
 
 
 app.listen(port, ()=> {
-  console.log(`Demo app is up and listening to port: ${port}`);
+  console.log(`Test app is up and listening to port: ${port}`);
 })
  
